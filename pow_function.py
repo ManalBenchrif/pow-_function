b2b05756174446ee1416c265e04a48206cf1c2c0
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-11-02 03:31:11 +0100

    Add files via upload

---
+++ pow_function.py
@@ -0,0 +1,5 @@
+#The pow() function returns the value of x to the power of y (xy).
+#If a third parameter is present, it returns x to the power of y, modulus z.
+
+print("2 a la puissance n: ",pow(2,3)) #(x^y).
+print("2 a la puissance n modulo 5: ",pow(2,3,5)) #(x^y) % z): %=rest
